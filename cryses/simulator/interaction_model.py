b0c8f572de1985049839d723c9bdad02239b382a
Shuo Zhang <imsure318@163.com>
2021-06-04 22:27:32 +0100

    Write agenda-based response generation pseudo code

--- cryses/simulator/interaction_model.py
+++ cryses/simulator/interaction_model.py
@@ -3,4 +3,29 @@ class InteractionModel:
 
     def __init__(self) -> None:
         """Initializes the interaction model."""
+        self._agenda = []
+        self._current_intent = None
         pass
+
+    def initialize_agenda(self):
+        """Initializes the action agenda."""
+        pass
+
+    @property
+    def agenda(self):
+        return self._agenda
+
+    @property
+    def current_intent(self):
+        return self._current_intent
+
+    def update_agenda(self, intent: str):
+        """Updates the agenda and determins the next user intent based on agent intent.
+
+        If agent replies with right intent in response to last user intent (checking mapping):
+            pops up the next user intent from the agenda;
+            update the current intent;
+        else:
+            push a new intent (select a replacement intent);
+            update the current intent;
+        """
