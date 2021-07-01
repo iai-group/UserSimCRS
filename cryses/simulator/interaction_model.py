d5927c9ca287f1f87ceb362df244f37ad1facc4d
Shuo Zhang <imsure318@163.com>
2021-07-01 14:50:07 +0100

    interaction model

--- cryses/simulator/interaction_model.py
+++ cryses/simulator/interaction_model.py
@@ -4,12 +4,17 @@
 
 import os
 import yaml
+import random
+from typing import List, Dict, Any, Tuple
 
 
 class InteractionModel:
     """Represents an interaction model."""
 
-    def __init__(self, config_file) -> None:
+    START_INTENT = "DISCLOSE.NON-DISCLOSE"
+    STOP_INTENT = "STOP"
+
+    def __init__(self, config_file, agenda_list: List[List]) -> None:
         """Initializes the interaction model."""
         # Load interaction model.
         if not os.path.isfile(config_file):
@@ -17,12 +22,54 @@ class InteractionModel:
         with open(config_file) as yaml_file:
             self._config = yaml.load(yaml_file, Loader=yaml.FullLoader)
 
+        (
+            self._user_intent_distribution,
+            self._intent_distribution,
+        ) = self.intent_distribution(agenda_list)
         # Initialize agenda.
-        self._agenda = []
+        self._agenda = self.initialize_agenda()
         # Keep track of the current user intent.
-        self._current_intent = None
+        self._current_intent = self._agenda.pop()
 
-    def initialize_agenda(self) -> None:
+    def intent_distribution(self, agenda_list: List[List]) -> Tuple[Dict, Dict]:
+        """Distill user intent distributions based on agenda list.
+
+        Arg:
+            Agenda_list: list of user agenda of intents.
+
+        Returns:
+            Intent distributions: {user of agent intent: {next_user_intent: occurrence}}
+        """
+        user_intent_dist, intent_dist = dict(), dict()
+        for agenda in agenda_list:
+            user_agenda = [i[1] for i in agenda if i[0] == "USER"]
+            for i, user_intent in enumerate(user_agenda):
+                if user_intent not in user_intent_dist:
+                    user_intent_dist[user_intent] = dict()
+                next_user_intent = (
+                    user_agenda[i + 1] if i < len(user_agenda) - 1 else self.STOP_INTENT
+                )
+                if next_user_intent not in user_intent_dist[user_intent]:
+                    user_intent_dist[user_intent][next_user_intent] = 0
+                user_intent_dist[user_intent][next_user_intent] += 1
+
+            for j, (tag, intent) in enumerate(agenda):
+                # Only consider
+                if tag != "AGENT":
+                    continue
+                if intent not in intent_dist:
+                    intent_dist[intent] = dict()
+                # TODO: consider the case when the next intent is not user intent.
+                next_user_intent = (
+                    agenda[j + 1][1] if j < len(agenda) - 1 else self.START_INTENT
+                )
+                if next_user_intent not in intent_dist[intent]:
+                    intent_dist[intent][next_user_intent] = 0
+                intent_dist[intent][next_user_intent] += 1
+
+        return user_intent_dist, intent_dist
+
+    def initialize_agenda(self) -> List:
         """Initializes the action agenda.
 
         Step1: Load all the dialogues with intents and generate a map:
@@ -45,7 +92,21 @@ class InteractionModel:
 
         Step3: filter the agenda, e.g. too short or too long agenda will triger this function rerun
         """
-        pass
+        current_intent_str = self.START_INTENT
+        agenda = list()
+        agenda.append(current_intent_str)
+        next_intent_str = self.next_intent(
+            current_intent_str, self._user_intent_distribution
+        )
+        while next_intent_str != self.STOP_INTENT:
+            current_intent_str = next_intent_str
+            agenda.append(current_intent_str)
+            next_intent_str = self.next_intent(
+                current_intent_str, self._user_intent_distribution
+            )
+        agenda.reverse()
+        self._agenda = agenda
+        return agenda
 
     @property
     def agenda(self):
@@ -77,16 +138,54 @@ class InteractionModel:
         """
         return agent_intent in self._config["agent_set_retrieval"]
 
-    def next_intent(self, intent: str) -> str:
+    def next_intent(self, intent_str: str, intent_dist: dict) -> str:
         """Predicts the next user intent.
 
-        Todo: sample the next intent from intent_map, e.g.,
-        Given current_intent, we determine the next intent (either next_intent1 or next_intent2) by probaliities.
+        Given current_intent, we determine the next intent (either next_intent1 or next_intent2) by probabilities.
 
+        Args:
+            Intent_str: str of current user intent.
+
+        Returns:
+            Next user intent string based on probability distribution.
         """
-        pass
+        # Get the distribution of next intent for the current user intent.
+        intent_map = intent_dist.get(intent_str)
+        assert isinstance(intent_map, dict)
 
-    def update_agenda(self, agent_intent: str) -> None:
+        # Randomly generates a probability from 0~1.
+        p_random = random.uniform(0, 1)
+
+        # Get the sum of the next intent occurences.
+        next_intent_occurences_sum = sum(intent_map.values())
+
+        # Get normalized next intent distribution occurences and next intent list.
+        d, next_intents = [], []
+        for next_intent, next_intent_occurrence in intent_map.items():
+            d.append(next_intent_occurrence / next_intent_occurences_sum)
+            next_intents.append(next_intent)
+        return self.p_sample(p_random, d, next_intents)
+
+    @staticmethod
+    def p_sample(p: float, d: List, items: List) -> Any:
+        """Determines the next item based on a randomly generated probability.
+
+        Args:
+            p: a randomly generated uniform probability.
+            d: list of probablitities of items.
+            items: items to be sampled from.
+
+        Return:
+            The sampled item.
+        """
+        p_start = 0
+        for i, p_item in enumerate(d):
+            p_start += p_item
+            if p < p_start:
+                return items[i]
+        return items[-1]
+
+    def update_agenda(self, agent_intent: str) -> str:
         """Updates the agenda and determines the next user intent based on agent
         intent.
 
@@ -100,6 +199,20 @@ class InteractionModel:
             updates the current intent.
 
         Args:
-            agent_intent: Agent's intent
+            Agent_intent: Agent's intent
+
+        Returns:
+            The current intent (the next intent).
         """
-        pass
+        expected_agent_intents = self._config.get("expected_responses").get(
+            self._current_intent
+        )
+        # If agent replies in an expected intent, then pop the next intent from agenda.
+        if agent_intent in expected_agent_intents:
+            self._current_intent = self._agenda.pop()
+        else:  # Find a replacement based on last agent intent
+            self._current_intent = self.next_intent(
+                agent_intent, self._intent_distribution
+            )
+        print(self._agenda.pop())
+        return self._current_intent
--- tests/simulator/test_interaction_model.py
+++ tests/simulator/test_interaction_model.py
@@ -5,10 +5,44 @@ import pytest
 from cryses.simulator.interaction_model import InteractionModel
 
 
+# List of user intents in agenda
+@pytest.fixture
+def agenda_list():
+    return [
+        [
+            ("USER", "DISCLOSE.NON-DISCLOSE"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-A"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-B"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-C"),
+        ],
+        [
+            ("USER", "DISCLOSE.NON-DISCLOSE"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-A"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-A"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-C"),
+        ],
+        [
+            ("USER", "DISCLOSE.NON-DISCLOSE"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-A"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-C"),
+            ("AGENT", "INQUIRE"),
+            ("USER", "Intent-C"),
+        ],
+    ]
+
+
 # CIR6 Interaction model.
 @pytest.fixture
-def im_cir6():
-    return InteractionModel("data/interaction_models/cir6.yaml")
+def im_cir6(agenda_list):
+    return InteractionModel("../../data/interaction_models/cir6.yaml", agenda_list)
 
 
 def test_is_agent_intent_elicit(im_cir6):
@@ -19,3 +53,42 @@ def test_is_agent_intent_elicit(im_cir6):
 def test_is_agent_intent_set_retrieval(im_cir6):
     assert im_cir6.is_agent_intent_set_retrieval("REVEAL")
     assert not im_cir6.is_agent_intent_set_retrieval("INQUIRE")
+
+
+def test_intent_distribution(im_cir6, agenda_list):
+    user_intent_distribution, intent_distribution = im_cir6.intent_distribution(
+        agenda_list
+    )
+    exepcted_user_intent_distribution = {
+        "DISCLOSE.NON-DISCLOSE": {"Intent-A": 3},
+        "Intent-A": {"Intent-B": 1, "Intent-A": 1, "Intent-C": 2},
+        "Intent-B": {"Intent-C": 1},
+        "Intent-C": {"STOP": 3, "Intent-C": 1},
+    }
+    exepcted_intent_distribution = {
+        "INQUIRE": {"Intent-A": 4, "Intent-B": 1, "Intent-C": 4}
+    }
+    assert user_intent_distribution == exepcted_user_intent_distribution
+    assert intent_distribution == exepcted_intent_distribution
+
+
+def test_next_intent(im_cir6, agenda_list):
+    user_intent_distribution, intent_distribution = im_cir6.intent_distribution(
+        agenda_list
+    )
+    assert im_cir6.next_intent("DISCLOSE.NON-DISCLOSE", user_intent_distribution)
+    assert im_cir6.next_intent("Intent-A", user_intent_distribution)
+    assert im_cir6.next_intent("Intent-C", user_intent_distribution)
+    assert im_cir6.next_intent("Intent-B", user_intent_distribution) == "Intent-C"
+
+
+def test_initialize_agenda(im_cir6):
+    assert len(im_cir6.initialize_agenda()) > 0
+    assert len(im_cir6.agenda) > 0
+    assert im_cir6.agenda[-1] == "DISCLOSE.NON-DISCLOSE"
+
+
+def test_update_agenda(im_cir6):
+    agenda_intent = "INQUIRE"
+    next_intent = im_cir6.update_agenda(agenda_intent)
+    assert next_intent
