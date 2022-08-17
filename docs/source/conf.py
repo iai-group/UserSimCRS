2ebdf63afa7eb481996cfd1df5492f1371a03ca6
adrzewiecki <aleksander.m.drzewiecki@gmail.com>
2022-08-17 08:31:46 +0200

    Add favicon to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -74,6 +74,7 @@ html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # so a file named 'default.css' will overwrite the builtin 'default.css'.
 html_static_path = ["_static"]
 html_theme_options = {}
+html_favicon = "_static/favicon.png"
 
 # Auto api
 autoapi_type = "python"
