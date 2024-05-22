42f8483508803bd80ecbbaec2a2836a535c9d41a
Nolwenn <28621493+NoB0@users.noreply.github.com>
2024-05-22 11:43:48 +0200

    Upgrade to DK 0.0.9

--- usersimcrs/run_simulation.py
+++ usersimcrs/run_simulation.py
@@ -4,42 +4,12 @@ import argparse
 import json
 import logging
 import os
-from typing import Set
 
 import confuse
-import requests
-import yaml
-from dialoguekit.connector.dialogue_connector import DialogueConnector
-from dialoguekit.core.dialogue import Dialogue
-from dialoguekit.core.domain import Domain
-from dialoguekit.core.intent import Intent
-from dialoguekit.core.utterance import Utterance
-from dialoguekit.nlg import ConditionalNLG
-from dialoguekit.nlg.template_from_training_data import (
-    extract_utterance_template,
-)
-from dialoguekit.nlu import NLU
-from dialoguekit.nlu.intent_classifier import IntentClassifier
-from dialoguekit.nlu.models.diet_classifier_rasa import IntentClassifierRasa
-from dialoguekit.nlu.models.intent_classifier_cosine import (
-    IntentClassifierCosine,
-)
-from dialoguekit.participant.agent import Agent
-from dialoguekit.participant.participant import DialogueParticipant
-from dialoguekit.platforms.platform import Platform
-from dialoguekit.utils.dialogue_reader import json_to_dialogues
-from sample_agents.moviebot_agent import MovieBotAgent
+from tqdm import tqdm
 
-from usersimcrs.items.item_collection import ItemCollection
-from usersimcrs.items.ratings import Ratings
-from usersimcrs.simulator.agenda_based.agenda_based_simulator import (
-    AgendaBasedSimulator,
-)
-from usersimcrs.simulator.agenda_based.interaction_model import InteractionModel
-from usersimcrs.simulator.user_simulator import UserSimulator
-from usersimcrs.user_modeling.simple_preference_model import (
-    SimplePreferenceModel,
-)
+import usersimcrs.utils.simulation_utils as utils
+from usersimcrs.simulation_platform import SimulationPlatform
 
 DEFAULT_CONFIG_PATH = "config/default/config_default.yaml"
 OUTPUT_DIR = "data/runs"
@@ -51,7 +21,7 @@ logging.basicConfig(
 logger = logging.getLogger()
 
 
-def main(config: confuse.Configuration, agent: Agent) -> None:
+def main(config: confuse.Configuration) -> None:
     """Executes the specified configuration.
 
     Loads domain and interaction model. Initializes agent and user. Runs the
@@ -60,88 +30,21 @@ def main(config: confuse.Configuration, agent: Agent) -> None:
     Args:
         config: Configuration generated from YAML configuration file.
         agent: Conversational agent.
-
-    Raises:
-        TypeError: if the agent is not an instance of Agent.
     """
-    if not isinstance(agent, Agent):
-        raise TypeError(f"agent must be Agent, not {type(agent).__name__}")
-
-    # Loads domain, item collection, and preference data
-    domain = Domain(config["domain"].get())
-
-    item_collection = ItemCollection()
-    item_collection.load_items_csv(
-        config["items"].get(),
-        domain=domain,
-        domain_mapping=config["domain_mapping"].get(),
-        id_col=config["id_col"].get(),
+    agent_class, agent_config = utils.get_agent_information(config)
+    simulator_id, simulator_class, simulator_config = (
+        utils.get_simulator_information(config)
     )
 
-    ratings = Ratings(item_collection)
-    ratings.load_ratings_csv(file_path=config["ratings"].get())
-    historical_ratings, ground_truth_ratings = ratings.create_split(
-        config["historical_ratings_ratio"].get(0.8)
-    )
+    platform = SimulationPlatform(agent_class, agent_config)
+    platform.start()
 
-    preference_model = SimplePreferenceModel(
-        domain,
-        item_collection,
-        historical_ratings,
-        historical_user_id="13",
-    )
-
-    # Loads dialogue sample
-    annotated_dialogues_file = config["dialogues"].get()
-    annotated_conversations = json_to_dialogues(
-        annotated_dialogues_file, agent_id=agent.id, user_id="User"
-    )
-
-    # Loads interaction model
-    interaction_model = InteractionModel(
-        config_file=config["intents"].get(),
-        annotated_conversations=annotated_conversations,
-    )
-
-    # NLU
-    nlu = get_NLU(config)
-
-    # NLG
-    template = extract_utterance_template(
-        annotated_dialogue_file=annotated_dialogues_file,
-    )
-    nlg = ConditionalNLG(template)
-
-    simulator = AgendaBasedSimulator(
-        config["simulator_id"].get(),
-        preference_model,
-        interaction_model,
-        nlu,
-        nlg,
-        domain,
-        item_collection,
-        ratings,
-    )
-    simulate_conversation(agent, simulator)
-
-
-def simulate_conversation(
-    agent: Agent, user_simulator: UserSimulator
-) -> Dialogue:
-    """Simulates a single conversation and returns the resulting dialogue.
-
-    Args:
-        agent: An agent.
-        user_simulator: A user simulator.
-
-    Returns:
-        The simulated dialogue.
-    """
-    platform = Platform()  # TODO: Add simulator platform
-    dc = DialogueConnector(agent, user_simulator, platform)
-    dc.start()
-    dc.close()
-    return dc.dialogue_history
+    for _ in tqdm(
+        range(config["num_simulated_dialogues"].get()),
+        desc=" Simulated dialogues",
+    ):
+        platform.connect(simulator_id, simulator_class, simulator_config)
+        platform.disconnect(simulator_id)
 
 
 def parse_args() -> argparse.Namespace:
@@ -253,99 +156,6 @@ def load_config(args: argparse.Namespace) -> confuse.Configuration:
     return config
 
 
-def get_NLU(config: confuse.Configuration) -> IntentClassifier:
-    """Returns an NLU component.
-
-    Only supports DialogueKit intent classifiers.
-
-    Args:
-        config: Configuration for the simulation.
-
-    Raises:
-        ValueError: Unsupported intent classifier.
-
-    Returns:
-        An NLU component.
-    """
-
-    intent_classifier = config["intent_classifier"].get()
-    if intent_classifier == "cosine":
-        # NLU without slot annotators
-        return NLU(load_cosine_classifier(config), slot_annotators=[])
-    elif intent_classifier == "diet":
-        classifier = load_rasa_diet_classifier(config)
-        return NLU(classifier, [classifier])
-    elif intent_classifier:
-        raise ValueError(
-            "Unsupported intent classifier. Check DialogueKit intent"
-            " classifiers."
-        )
-
-
-def load_cosine_classifier(
-    config: confuse.Configuration,
-) -> IntentClassifierCosine:
-    """Trains a cosine classifier on annotated dialogues for NLU module.
-
-    Args:
-        config: Configuration generated from YAML configuration file.
-
-    Returns:
-        A trained cosine model for intent classification.
-    """
-    # TODO: Move to DialogueKit as util function.
-    # See: https://github.com/iai-group/UserSimCRS/issues/92
-    annotated_dialogues_file = config["dialogues"].get()
-    dialogues = json.load(open(annotated_dialogues_file))
-
-    gt_intents = []
-    utterances = []
-    for conversation in dialogues:
-        for turn in conversation["conversation"]:
-            if turn["participant"] == "AGENT":
-                gt_intents.append(Intent(turn["intent"]))
-                utterances.append(
-                    Utterance(
-                        turn["utterance"], participant=DialogueParticipant.AGENT
-                    )
-                )
-    intent_classifier = IntentClassifierCosine(intents=gt_intents)
-    intent_classifier.train_model(utterances=utterances, labels=gt_intents)
-    return intent_classifier
-
-
-def load_rasa_diet_classifier(
-    config: confuse.Configuration,
-) -> IntentClassifierCosine:
-    """Trains a DIET classifier on Rasa annotated dialogues for NLU module.
-
-    Args:
-        config: Configuration generated from YAML configuration file.
-
-    Returns:
-        A trained Rasa DIET model for intent classification.
-    """
-    # TODO: Move to DialogueKit as util function.
-    # See: https://github.com/iai-group/UserSimCRS/issues/92
-    intent_schema_file = config["intents"].get()
-    intent_schema = yaml.load(open(intent_schema_file), Loader=yaml.FullLoader)
-
-    agent_intents_str: Set[str] = set()
-    for v in intent_schema["user_intents"].values():
-        intents = v.get("expected_agent_intents", []) or []
-        agent_intents_str.update(intents)
-    # agent_intents_str = intent_schema["agent_elicit_intents"]
-    # agent_intents_str.extend(intent_schema["agent_set_retrieval"])
-    agent_intents = [Intent(intent) for intent in agent_intents_str]
-    intent_classifier = IntentClassifierRasa(
-        agent_intents,
-        config["rasa_dialogues"].get(),
-        ".rasa",
-    )
-    intent_classifier.train_model()
-    return intent_classifier
-
-
 if __name__ == "__main__":
     args = parse_args()
     config = load_config(args)
@@ -353,19 +163,4 @@ if __name__ == "__main__":
     if config["debug"].get():
         logger.setLevel(logging.DEBUG)
 
-    # Defines the agent for the simulation.
-    # By default, a local moviebot is used.
-    # See usage example in README for more details.
-    agent_uri = config["agent_uri"].get()
-    try:
-        response = requests.get(agent_uri, timeout=60)
-        assert response.status_code == 200
-        agent = MovieBotAgent(agent_id=config["agent_id"].get(), uri=agent_uri)
-    except requests.exceptions.RequestException:
-        raise RuntimeError(
-            f"Connection refused to {agent_uri}. Please check that "
-            "the conversational agent is running at this address. See the full "
-            "traceback above."
-        )
-
-    main(config, agent)
+    main(config)
