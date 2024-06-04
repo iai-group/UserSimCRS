1c5b48adc4324678a57a6e61104fa13638342f63
Nolwenn Bernard <28621493+NoB0@users.noreply.github.com>
2024-06-04 09:34:35 +0200

    Add dialogue state and dialogue state tracker (#164)
    * Add dialogue state and dialogue state tracker
    
    * Add tests for DST
    
    * Black
    
    * Fix pre-commit
    
    * Address review comments
    
    * Update dialogue state
    
    * Address review comment

---
+++ usersimcrs/dialogue_management/dialogue_state.py
@@ -0,0 +1,32 @@
+"""Representation of the dialogue state.
+
+The dialogue state includes the number of utterances, a list of dialogue acts
+per utterance for both the agent and the user, and the belief state. The belief
+state is a dictionary that holds for each slot (key) the values provided by the
+user.
+"""
+
+from collections import defaultdict
+from dataclasses import dataclass, field
+from typing import DefaultDict, List
+
+from dialoguekit.core.dialogue_act import DialogueAct
+
+
+@dataclass
+class DialogueState:
+    """Dialogue state.
+
+    Attributes:
+        utterance_count: Utterance count.
+        agent_dialogue_acts: List of dialogue acts per turn for the agent.
+        user_dialogue_acts: List of dialogue acts per turn for the user.
+        belief_state: Belief state.
+    """
+
+    utterance_count: int = 0
+    agent_dialogue_acts: List[List[DialogueAct]] = field(default_factory=list)
+    user_dialogue_acts: List[List[DialogueAct]] = field(default_factory=list)
+    belief_state: DefaultDict[str, List[str]] = field(
+        default_factory=lambda: defaultdict(list)
+    )
