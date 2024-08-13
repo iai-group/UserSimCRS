7f4245962619c4ec49f5685e0d33fd33cdb93bca
Nolwenn Bernard <28621493+NoB0@users.noreply.github.com>
2024-08-13 17:13:50 +0200

    Implement LLM-based user simulator (#167)
    * Add interfaces to access hosted LLMs
    
    * Add single prompt US
    
    * Add information need to US
    
    * Add prompt
    
    * Update LLM-based US initialization
    
    * Add tests for prompt
    
    * Add ollama to requirements
    
    * Fix pre-commit
    
    * Fix argument order openai
    
    * Fix utterance initialization
    
    * Address review comment
    
    * Fix mypy issue

---
+++ usersimcrs/simulator/llm/interfaces/ollama_interface.py
@@ -0,0 +1,70 @@
+"""Interface to use a LLM served by Ollama."""
+
+import os
+
+import yaml
+from ollama import Client, Options
+
+from dialoguekit.core import Utterance
+from dialoguekit.participant import DialogueParticipant
+from usersimcrs.simulator.llm.interfaces.llm_interface import LLMInterface
+from usersimcrs.simulator.llm.prompt import Prompt
+
+
+class OllamaLLMInterface(LLMInterface):
+    def __init__(
+        self,
+        configuration_path: str,
+        default_response: str = None,
+    ) -> None:
+        """Initializes interface for ollama served LLM.
+
+        Args:
+            configuration_path: Path to the configuration file.
+            default_response: Default response to be used if the LLM fails to
+              generate a response.
+
+        Raises:
+            FileNotFoundError: If the configuration file is not found.
+            ValueError: If the model or host is not specified in the config.
+        """
+        super().__init__(default_response)
+        if not os.path.exists(configuration_path):
+            raise FileNotFoundError(
+                f"Configuration file not found: {configuration_path}"
+            )
+
+        with open(configuration_path, "r") as f:
+            self._llm_configuration = yaml.safe_load(f)
+
+        if "model" not in self._llm_configuration:
+            raise ValueError(
+                "No model specified in the config, e.g., 'llama2'."
+            )
+        if "host" not in self._llm_configuration:
+            raise ValueError("No host specified in the config.")
+
+        self.client = Client(host=self._llm_configuration.get("host"))
+        self.model = self._llm_configuration.get("model")
+        self._stream = self._llm_configuration.get("stream", False)
+        self._llm_options = Options(
+            **self._llm_configuration.get("options", {})
+        )
+
+    def generate_response(self, prompt: Prompt) -> Utterance:
+        """Generates a user utterance given a prompt.
+
+        Args:
+            prompt: Prompt for generating the utterance.
+
+        Returns:
+            Utterance.
+        """
+        ollama_response = self.client.generate(
+            self.model,
+            prompt.prompt_text,
+            options=self._llm_options,
+            stream=self._stream,
+        )
+        response = ollama_response.get("text", self.default_response)
+        return Utterance(response, participant=DialogueParticipant.USER)
