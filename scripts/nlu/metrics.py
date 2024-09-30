02de6cb38b9376c37edede737af78e85c13ce245
Nolwenn <28621493+NoB0@users.noreply.github.com>
2024-09-30 17:22:52 +0200

    Address review comments

--- scripts/nlu/metrics.py
+++ scripts/nlu/metrics.py
@@ -1,11 +1,11 @@
 """Define the evaluation metric for the dialogue act extraction.
 
 We define 5 metrics:
-1. Slot Error Rate (SER)
-2. Intent Error Rate (IER)
-3. Dialogue Acts Recall (DAR)
-4. Dialogue Acts Precision (DAP)
-5. Dialogue Acts F1 Score (DAF1)
+1. Slot Error Rate (ER_slot)
+2. Intent Error Rate (ER_intent)
+3. Dialogue Acts Recall (Recall_DA)
+4. Dialogue Acts Precision (Prec_DA)
+5. Dialogue Acts F1 Score (F1_DA)
 """
 
 from typing import List, Tuple
@@ -33,14 +33,14 @@ def slot_error_rate(
     predicted_dialogue_acts: List[DialogueAct],
     target_dialogue_acts: List[DialogueAct],
 ) -> float:
-    """Calculates the Slot Error Rate (SER).
+    """Calculates the Slot Error Rate (ER_slot).
 
     Args:
         predicted_dialogue_acts: Predicted dialogue acts.
         target_dialogue_acts: Target dialogue acts.
 
     Returns:
-        Slot Error Rate (SER).
+        Slot Error Rate.
     """
     predicted_slot_value_pairs = _get_slot_value_pairs(predicted_dialogue_acts)
     target_slot_value_pairs = _get_slot_value_pairs(target_dialogue_acts)
@@ -58,14 +58,14 @@ def intent_error_rate(
     predicted_dialogue_acts: List[DialogueAct],
     target_dialogue_acts: List[DialogueAct],
 ) -> float:
-    """Calculates the Intent Error Rate (IER).
+    """Calculates the Intent Error Rate (ER_intent).
 
     Args:
         predicted_dialogue_acts: Predicted dialogue acts.
         target_dialogue_acts: Target dialogue acts.
 
     Returns:
-        Intent Error Rate (IER).
+        Intent Error Rate.
     """
     predicted_intents = _get_intents(predicted_dialogue_acts)
     target_intents = _get_intents(target_dialogue_acts)
@@ -81,14 +81,14 @@ def dialogue_acts_recall(
     predicted_dialogue_acts: List[DialogueAct],
     target_dialogue_acts: List[DialogueAct],
 ) -> float:
-    """Calculates the Dialogue Acts Recall (DAR).
+    """Calculates the Dialogue Acts Recall (Recall_DA).
 
     Args:
         predicted_dialogue_acts: Predicted dialogue acts.
         target_dialogue_acts: Target dialogue acts.
 
     Returns:
-        Dialogue Acts Recall (DAR).
+        Dialogue Acts Recall.
     """
     num_correct_dialogue_acts = len(
         set(predicted_dialogue_acts) & set(target_dialogue_acts)
@@ -103,14 +103,14 @@ def dialogue_acts_precision(
     predicted_dialogue_acts: List[DialogueAct],
     target_dialogue_acts: List[DialogueAct],
 ) -> float:
-    """Calculates the Dialogue Acts Precision (DAP).
+    """Calculates the Dialogue Acts Precision (Prec_DA).
 
     Args:
         predicted_dialogue_acts: Predicted dialogue acts.
         target_dialogue_acts: Target dialogue acts.
 
     Returns:
-        Dialogue Acts Precision (DAP).
+        Dialogue Acts Precision.
     """
     num_correct_dialogue_acts = len(
         set(predicted_dialogue_acts) & set(target_dialogue_acts)
@@ -125,16 +125,18 @@ def dialogue_acts_f1_score(
     predicted_dialogue_acts: List[DialogueAct],
     target_dialogue_acts: List[DialogueAct],
 ) -> float:
-    """Calculates the Dialogue Acts F1 Score (DAF1).
+    """Calculates the Dialogue Acts F1 Score (F1_DA).
 
     Args:
         predicted_dialogue_acts: Predicted dialogue acts.
         target_dialogue_acts: Target dialogue acts.
 
     Returns:
-        Dialogue Acts F1 Score (DAF1).
+        Dialogue Acts F1 Score.
     """
-    recall = dialogue_acts_recall(predicted_dialogue_acts, target_dialogue_acts)
+    recall = dialogue_acts_recall(
+        predicted_dialogue_acts, target_dialogue_acts
+    )
     precision = dialogue_acts_precision(
         predicted_dialogue_acts, target_dialogue_acts
     )
