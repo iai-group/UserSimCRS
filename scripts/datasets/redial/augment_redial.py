f5bfd46a75c5fdba0023504ecffd43e0f87ac652
Nolwenn Bernard <28621493+NoB0@users.noreply.github.com>
2024-10-01 16:13:59 +0200

    Feature/149-Add-benchmark-datasets (#157)
    * Reorganize data folder
    
    * Add script to format ReDial dataset to DK format
    
    * Fix pre-commit
    
    * Add augmentation script
    
    * Remove unnecessary comments
    
    * Formatting
    
    * Add type annotation
    
    * Add serialization to IN
    
    * Reorganize scripts
    
    * Quick fix
    
    * Address review comments

---
+++ scripts/datasets/redial/augment_redial.py
@@ -0,0 +1,143 @@
+"""Augment the formatted ReDial dataset with additional information.
+
+The augmentation is artificial and done to create training data for neural user
+simulators. Each dialogue is augmented with an information need and each
+utterance is augmented with dialogue acts. The information need is inferred from
+the dialogue acts, e.g., the annotations of a inquire dialogue act can serve as
+requests.
+"""
+
+import argparse
+import json
+
+from tqdm import tqdm
+
+from dialoguekit.core.dialogue import Dialogue
+from dialoguekit.nlu.nlu import NLU
+from dialoguekit.participant.participant import DialogueParticipant
+from dialoguekit.utils.dialogue_reader import json_to_dialogues
+from scripts.datasets.information_need_annotation.information_need_annotator import (  # noqa: E501
+    InformationNeedAnnotator,
+)
+from usersimcrs.nlu.lm.lm_dialogue_act_extractor import LMDialogueActsExtractor
+
+
+def annotate_dialogue_acts(
+    user_nlu: NLU, agent_nlu: NLU, dialogue: Dialogue
+) -> Dialogue:
+    """Annotates the dialogue acts in the dialogue.
+
+    Args:
+        user_nlu: NLU to use for user dialogue act annotation.
+        agent_nlu: NLU to use for agent dialogue act annotation.
+        dialogue: Dialogue to annotate.
+
+    Returns:
+        Dialogue with annotated dialogue acts.
+    """
+    for utterance in dialogue.utterances:
+        if utterance.participant == DialogueParticipant.USER:
+            dialogue_acts = user_nlu.extract_dialogue_acts(utterance)
+        else:
+            dialogue_acts = agent_nlu.extract_dialogue_acts(utterance)
+        utterance.add_dialogue_acts(dialogue_acts)
+    return dialogue
+
+
+def augment_dialogue(
+    user_nlu: NLU,
+    agent_nlu: NLU,
+    information_need_annotator: InformationNeedAnnotator,
+    dialogue: Dialogue,
+) -> Dialogue:
+    """Augments a dialogue with dialogue acts and information need.
+
+    Args:
+        user_nlu: NLU to use for user dialogue act annotation.
+        agent_nlu: NLU to use for agent dialogue act annotation.
+        information_need_annotator: Information need annotator.
+        dialogue: Dialogue to augment.
+
+    Returns:
+        Augmented dialogue.
+    """
+    dialogue = annotate_dialogue_acts(user_nlu, agent_nlu, dialogue)
+    dialogue = information_need_annotator.annotate_information_need(dialogue)
+    return dialogue
+
+
+def parse_args() -> argparse.Namespace:
+    """Parses command-line arguments.
+
+    Returns:
+        Parsed command-line arguments.
+    """
+    parser = argparse.ArgumentParser(
+        description=(
+            "Augment the formatted ReDial dataset with additional information."
+        ),
+        prog="augment_redial.py",
+    )
+    parser.add_argument(
+        "--input_path",
+        type=str,
+        default="data/datasets/redial/train_dialogues.json",
+        help="Path to the formatted ReDial dataset.",
+    )
+    parser.add_argument(
+        "--user_nlu_config",
+        type=str,
+        default="config/nlu/user_dialogue_acts_extraction_config_default.yaml",
+        help="NLU configuration file for user dialogue acts extraction.",
+    )
+    parser.add_argument(
+        "--agent_nlu_config",
+        type=str,
+        default="config/nlu/agent_dialogue_acts_extraction_config_default.yaml",
+        help="NLU configuration file for agent dialogue acts extraction.",
+    )
+    parser.add_argument(
+        "--ollama_config",
+        type=str,
+        default="config/llm_interface/config_ollama_information_need.yaml",
+        help="Configuration file for Ollama.",
+    )
+    parser.add_argument(
+        "--information_need_prompt",
+        type=str,
+        default="scripts/datasets/information_need_annotation/information_need_prompt_movies_default.txt",  # noqa: E501
+        help="File containing the prompt for information need annotation.",
+    )
+    parser.add_argument(
+        "--output_path",
+        type=str,
+        default="data/datasets/redial/augmented_train_dialogues.json",
+        help="Path to save the augmented dataset.",
+    )
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    dialogues = json_to_dialogues(args.input_path)
+    user_nlu = NLU(LMDialogueActsExtractor(args.user_nlu_config))
+    agent_nlu = NLU(LMDialogueActsExtractor(args.agent_nlu_config))
+
+    information_need_annotator = InformationNeedAnnotator(
+        args.ollama_config, args.information_need_prompt
+    )
+
+    augmented_dialogues = [
+        augment_dialogue(
+            user_nlu, agent_nlu, information_need_annotator, dialogue
+        )
+        for dialogue in tqdm(dialogues)
+    ]
+
+    with open(args.output_path, "w") as output_file:
+        json.dump(
+            [dialogue.to_dict() for dialogue in augmented_dialogues],
+            output_file,
+            indent=4,
+        )
---
+++ scripts/datasets/redial/redial_to_dialoguekit.py
@@ -0,0 +1,286 @@
+"""Script to format the ReDial dataset into DialogueKit format.
+
+This script downloads the ReDial dataset and formats the dialogues following
+DialogueKit's format. Formatting includes merging consecutive utterances from
+the same participant and replacing movie mentions with their titles and years.
+In addition to formatting the dialogues, this script also processes items and
+ratings information.
+"""
+
+import json
+import logging
+import os
+import re
+import zipfile
+from copy import deepcopy
+from typing import Any, Dict, List, Tuple
+
+import pandas as pd
+import wget
+
+REDIAL_DATASET_URL = (
+    "https://github.com/ReDialData/website/raw/data/redial_dataset.zip"
+)
+DATASET_PATH = "data/datasets/redial"
+ITEM_COLLECTION_PATH = "data/item_collections/redial"
+
+Items = Dict[str, Dict[str, Any]]
+Rating = Tuple[str, str, float]
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+logger.addHandler(stream_handler)
+
+
+def download_redial_dataset(
+    dataset_url: str = REDIAL_DATASET_URL, dataset_path: str = DATASET_PATH
+) -> None:
+    """Downloads the ReDial dataset from the official source.
+
+    Args:
+        dataset_url: URL of the ReDial dataset.
+        dataset_path: Path to save the ReDial dataset.
+    """
+    wget.download(dataset_url, out="scripts/redial_dataset.zip")
+
+    with zipfile.ZipFile("scripts/redial_dataset.zip", "r") as zip_folder:
+        zip_folder.extractall(dataset_path)
+
+    os.remove("scripts/redial_dataset.zip")
+
+
+def load_data(
+    path: str,
+    items: Items = None,
+    ratings: List[Rating] = None,
+) -> Tuple[List[Dict[str, Any]], Items, List[Rating]]:
+    """Loads dialogues from a JSONL file.
+
+    Args:
+        path: Path to the JSONL file.
+        items: Items mentioned in the dialogues.
+        ratings: Ratings extracted from the dialogues.
+
+    Returns:
+        List of dialogues, items mentioned in the dialogues, and ratings.
+    """
+    data = []
+    items = deepcopy(items) if items else dict()
+    ratings = deepcopy(ratings) if ratings else list()
+
+    for line in open(path, "r"):
+        dialogue, _items, _ratings = format_dialogue(json.loads(line))
+        data.append(dialogue)
+        items.update(_items)
+        ratings.extend(_ratings)
+    return data, items, ratings
+
+
+def format_dialogue(
+    dialogue: Dict[str, Any]
+) -> Tuple[Dict[str, Any], Items, List[Rating]]:
+    """Formats a dialogue into DialogueKit format.
+
+    Args:
+        dialogue: Dialogue to format.
+
+    Returns:
+        Formatted dialogue, items mentioned in the dialogue, and ratings.
+    """
+    formatted_dialogue = {
+        "conversation_id": dialogue["conversationId"],
+        "agent": {
+            "id": dialogue["respondentWorkerId"],
+            "type": "AGENT",
+        },
+        "user": {
+            "id": dialogue["initiatorWorkerId"],
+            "type": "USER",
+        },
+    }
+    items = get_items(dialogue)
+    ratings = get_ratings(dialogue, dialogue["initiatorWorkerId"])
+    utterances = parse_utterances(
+        dialogue, dialogue["initiatorWorkerId"], items
+    )
+    formatted_dialogue["conversation"] = utterances
+    return formatted_dialogue, items, ratings
+
+
+def parse_utterances(
+    dialogue: Dict[str, Any], user_id: str, items: Items
+) -> List[Dict[str, Any]]:
+    """Parses utterances from the dialogue.
+
+    Args:
+        dialogue: Dialogue to parse utterances from.
+        user_id: ID of the user in the dialogue.
+        items: Items mentioned in the dialogue.
+
+    Returns:
+        List of parsed utterances.
+    """
+    utterances = []
+    for message in dialogue["messages"]:
+        participant = (
+            "USER" if message["senderWorkerId"] == user_id else "AGENT"
+        )
+        text = message["text"]
+        utterance = {
+            "participant": participant,
+            "timeOffset": message["timeOffset"],
+        }
+
+        pattern = r"(@\d+)"
+        matches = re.findall(pattern, text)
+        if matches:
+            for m in matches:
+                movie_id = m[1:]
+                movie_title = items.get(movie_id, {}).get("title", None)
+                if movie_title:
+                    movie_year = items.get(movie_id, {}).get("year", None)
+                    movie = (
+                        f"{movie_title} ({movie_year})"
+                        if movie_year
+                        else movie_title
+                    )
+                    text = text.replace(m, movie)
+
+        utterance["utterance"] = text
+        utterances.append(utterance)
+
+    utterances = _merge_consecutive_utterances(utterances)
+    return utterances
+
+
+def _merge_consecutive_utterances(
+    utterances: List[Dict[str, Any]]
+) -> List[Dict[str, Any]]:
+    """Merges consecutive utterances from the same participant.
+
+    Args:
+        utterances: List of utterances to merge.
+
+    Returns:
+        List of merged utterances.
+    """
+    merged_utterances: List[Dict[str, Any]] = []
+    for utterance in utterances:
+        if (
+            not merged_utterances
+            or utterance["participant"] != merged_utterances[-1]["participant"]
+        ):
+            merged_utterances.append(utterance)
+        else:
+            merged_utterances[-1]["utterance"] += "\n" + utterance["utterance"]
+    return merged_utterances
+
+
+def get_items(dialogue: Dict[str, Any]) -> Items:
+    """Extracts items mentioned in the dialogue.
+
+    Args:
+        dialogue: Dialogue to extract items from.
+
+    Raises:
+        AttributeError: If no movie mentions found.
+
+    Returns:
+        Items mentioned in the dialogue.
+    """
+    items = {}
+    pattern = r"(?P<title>.+)\s\((?P<year>\d{4})\)"
+    try:
+        for id, value in dialogue["movieMentions"].items():
+            if not value:
+                continue
+            match = re.search(pattern, value)
+            if match:
+                title = match.group("title").strip()
+                year = int(match.group("year"))
+                items[id] = {"title": title, "year": year}
+    except AttributeError as e:
+        # No movie mentions found.
+        logger.error(e)
+
+    return items
+
+
+def get_ratings(dialogue: Dict[str, Any], user_id: str) -> List[Rating]:
+    """Extracts ratings from the dialogue.
+
+    Args:
+        dialogue: Dialogue to extract ratings from.
+        user_id: ID of the user who provided the ratings.
+
+    Raises:
+        AttributeError: If no questions found.
+
+    Returns:
+        List of ratings extracted from the dialogue.
+    """
+    ratings = list()
+    for q in ["initiatorQuestions", "respondentQuestions"]:
+        try:
+            for id, value in dialogue.get(q, {}).items():
+                rating = value["liked"]
+                if rating != 2:
+                    ratings.append((user_id, id, rating))
+        except AttributeError as e:
+            # No questions found.
+            logger.error(e)
+    return ratings
+
+
+if __name__ == "__main__":
+    for p in [DATASET_PATH, ITEM_COLLECTION_PATH]:
+        if not os.path.exists(p):
+            os.makedirs(DATASET_PATH, exist_ok=True)
+
+    download_redial_dataset()
+
+    items: Items = {}
+    ratings: List[Rating] = []
+
+    train_data, items, ratings = load_data(
+        os.path.join(DATASET_PATH, "train_data.jsonl"), items, ratings
+    )
+    test_data, items, ratings = load_data(
+        os.path.join(DATASET_PATH, "test_data.jsonl"), items, ratings
+    )
+
+    # Save formatted dialogues
+    with open(os.path.join(DATASET_PATH, "train_dialogues.json"), "w") as f:
+        json.dump(train_data, f, indent=4)
+        logger.info(f"Saved {len(train_data)} training dialogues in: {f.name}")
+
+    with open(os.path.join(DATASET_PATH, "test_dialogues.json"), "w") as f:
+        json.dump(test_data, f, indent=4)
+        logger.info(f"Saved {len(test_data)} test dialogues in: {f.name}")
+
+    with open(os.path.join(DATASET_PATH, "all_dialogues.json"), "w") as f:
+        json.dump(train_data + test_data, f, indent=4)
+
+    # Save items
+    items_df = pd.DataFrame(items).T.reset_index()
+    items_df.columns = ["item_id", "title", "year"]
+    items_df.drop_duplicates(subset=["item_id"], inplace=True)
+    logger.info(f"Collected {len(items_df)} items.")
+    items_df.to_csv(
+        os.path.join(ITEM_COLLECTION_PATH, "movies.csv"), index=False
+    )
+
+    # Save ratings
+    ratings_df = pd.DataFrame(
+        ratings,
+        columns=["user_id", "item_id", "rating"],
+    )
+    ratings_df.drop_duplicates(inplace=True)
+    logger.info(f"Collected {len(ratings_df)} ratings.")
+    ratings_df.to_csv(
+        os.path.join(ITEM_COLLECTION_PATH, "ratings.csv"), index=False
+    )
