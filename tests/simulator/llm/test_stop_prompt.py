faa47b4971ad5116f5ade734a3bc3d410e55a80f
Nolwenn <28621493+NoB0@users.noreply.github.com>
2025-11-18 09:48:44 +0100

    Update tests

--- tests/simulator/llm/test_stop_prompt.py
+++ tests/simulator/llm/test_stop_prompt.py
@@ -17,24 +17,15 @@ def prompt(information_need: InformationNeed) -> StopPrompt:
 
 def test_build_new_prompt(prompt: StopPrompt) -> None:
     """Tests the build_new_prompt method."""
-    stringified_requirements = (
-        "\nREQUIREMENTS: You are looking for a item with the following "
-        "characteristics: genre=Comedy, director=Steven Spielberg and want to "
-        "know the following information about it: plot, rating.\nHISTORY:\n"
-    )
-
     assert prompt.build_new_prompt() == (
-        DEFAULT_STOP_DEFINITION + stringified_requirements
+        DEFAULT_STOP_DEFINITION + "\nHISTORY:\n"
     )
 
 
 def test_prompt_text(prompt: StopPrompt) -> None:
     """Tests the prompt_text property."""
     initial_prompt = (
-        f"{DEFAULT_STOP_DEFINITION}\nREQUIREMENTS: You are looking "
-        "for a item with the following characteristics: genre=Comedy, "
-        "director=Steven Spielberg and want to know the following information "
-        "about it: plot, rating.\nHISTORY:\n"
+        f"{DEFAULT_STOP_DEFINITION}\nHISTORY:\n"
     )
 
     assert prompt.prompt_text == initial_prompt + "\n\nCONTINUE: "
