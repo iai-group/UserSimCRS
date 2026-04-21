bdf46b02f240bd73653e140671f2b7322f3e09b2
Ksenia Blokhina <kseniablokhina@MacBook-Pro-Ksenia.local>
2026-04-21 11:23:28 +0200

    update tests and logic

--- tests/evaluation/test_success_rate_metric.py
+++ tests/evaluation/test_success_rate_metric.py
@@ -59,11 +59,115 @@ def test_success_rate_metric_accepted_round_with_rejection(
     acceptance_intents: List[Intent],
     rejection_intents: List[Intent],
 ) -> None:
+    """Verifies that an accepted recommendation yields a success rate of 0."""
+    dialogue = Dialogue(agent_id="Agent", user_id="User", conversation_id="cid")
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="rec",
+            participant=DialogueParticipant.AGENT,
+            dialogue_acts=[DialogueAct(Intent("REC-S"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="accept",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("ACC"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="no",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("REJ"))],
+        )
+    )
+
+    assert (
+        success_rate_metric.evaluate_dialogue(
+            dialogue,
+            recommendation_intents,
+            acceptance_intents,
+            rejection_intents,
+        )
+        == 0.0
+    )
+
+
+def test_success_rate_metric_accepted_round_with_acceptance(
+    success_rate_metric: SuccessRateMetric,
+    recommendation_intents: List[Intent],
+    acceptance_intents: List[Intent],
+    rejection_intents: List[Intent],
+) -> None:
     """Verifies that an accepted recommendation yields a success rate of 1."""
     dialogue = Dialogue(agent_id="Agent", user_id="User", conversation_id="cid")
     dialogue.add_utterance(
         AnnotatedUtterance(
             text="rec",
+            participant=DialogueParticipant.AGENT,
+            dialogue_acts=[DialogueAct(Intent("REC-S"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="no",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("REJ"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="accept",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("ACC"))],
+        )
+    )
+
+    assert (
+        success_rate_metric.evaluate_dialogue(
+            dialogue,
+            recommendation_intents,
+            acceptance_intents,
+            rejection_intents,
+        )
+        == 1.0
+    )
+
+
+def test_success_rate_metric_accepted_2_rounds(
+    success_rate_metric: SuccessRateMetric,
+    recommendation_intents: List[Intent],
+    acceptance_intents: List[Intent],
+    rejection_intents: List[Intent],
+) -> None:
+    """Verifies that an accepted recommendation yields a success rate of 1."""
+    dialogue = Dialogue(agent_id="Agent", user_id="User", conversation_id="cid")
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="rec",
+            participant=DialogueParticipant.AGENT,
+            dialogue_acts=[DialogueAct(Intent("REC-S"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="no",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("REJ"))],
+        )
+    )
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="accept",
+            participant=DialogueParticipant.USER,
+            dialogue_acts=[DialogueAct(Intent("ACC"))],
+        )
+    )
+    # 2 round
+    dialogue.add_utterance(
+        AnnotatedUtterance(
+            text="rec2",
             participant=DialogueParticipant.AGENT,
             dialogue_acts=[DialogueAct(Intent("REC-S"))],
         )
